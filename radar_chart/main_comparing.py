ac18636953a20b1b0942943f8a3ccba35a6860d1
GavrikGal <gavrik.gal@gmail.com>
2023-07-28 14:22:20 +0300

    STAT-2. Реализовано построение графика сравнения

--- radar_chart/main_comparing.py
+++ radar_chart/main_comparing.py
@@ -4,8 +4,9 @@ from utils.services import RadarData, RadarPlotter
 
 
 DIR_NAME = r'd:\WorkSpace\Python\pythonProject\Statistic\data\ВМЦ-61.2ЖК\1903103\Доработка 2 (Поменяли стекло)\LVDS ВП'
+DIR2_NAME = None
 
-# TODO: Добавить возможность расчета из другой папки со сравнением на соответствующих графиках
+# Если надо отрубить второй набор данных, то комментить следующую строчку
 DIR2_NAME =r'd:\WorkSpace\Python\pythonProject\Statistic\data\ВМЦ-61.2ЖК\1903103\Доработка 3 (Матрица обклеена лентой)\LVDS ВП'
 
 if __name__ == '__main__':
@@ -13,10 +14,11 @@ if __name__ == '__main__':
     matplotlib.use('TkAgg')
 
     data1 = RadarData(DIR_NAME)
-    data2 = RadarData(DIR2_NAME)
+    if DIR2_NAME is not None:
+        data2 = RadarData(DIR2_NAME)
+    else:
+        data2 = None
 
     plotter = RadarPlotter(data1, data2)
     plotter.show()
     plotter.save()
-
-
