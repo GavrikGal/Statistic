7d9aa159a8b72baec933fea42242965adb116aa2
GavrikGal <gavrik.gal@gmail.com>
2023-08-04 10:08:18 +0300

    STAT-12 Smoke-тест

--- radar_chart/tests/test_levels.py
+++ radar_chart/tests/test_levels.py
@@ -10,6 +10,7 @@ class TestLevels(unittest.TestCase):
     def test_smoke_radar_data(self):
         """Тестируем, что данные вообще обрабатываются и объект класса создается"""
 
+        ...
         path = pathlib.Path(r'data/DataSet 1/DVI ВП').resolve()
         radar_data = RadarDataLevels(str(path))
         self.assertIsNotNone(radar_data)
