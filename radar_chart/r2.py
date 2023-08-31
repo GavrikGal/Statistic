efc1d619c6f4e1f843f12e263791c6889cfdf541
GavrikGal <gavrik.gal@gmail.com>
2023-08-31 08:36:06 +0300

    STAT-10 Рефакторинг. Пересмотр архитектуры приложения

--- radar_chart/r2.py
+++ radar_chart/r2.py
@@ -4,11 +4,11 @@ from radarplot.radar_data import RadarDataR2
 from radarplot.plotter import RadarR2Plotter
 
 
-DIR_NAME = r'd:\WorkSpace\Python\pythonProject\Statistic\data\ВМЦ-61.2ЖК\1903103\Доработка 4 (Стекло НВИТ...317)\DVI max'
+DIR_NAME = r'd:\WorkSpace\Python\pythonProject\Statistic\data\ВМЦ-61.2ЖК\Монитор без краски\Исходные\DVI max'
 DIR2_NAME = None
 
 # Если надо отрубить второй набор данных, то комментить следующую строчку
-DIR2_NAME = r'd:\WorkSpace\Python\pythonProject\Statistic\data\ВМЦ-61.2ЖК\1903103\Доработка 5 (Уплотнители в задней части корпуса + дораб LVDS)\DVI max'
+# DIR2_NAME = r'd:\WorkSpace\Python\pythonProject\Statistic\data\ВМЦ-61.2ЖК\1903103\Доработка 5 (Уплотнители в задней части корпуса + дораб LVDS)\DVI max'
 
 if __name__ == '__main__':
     matplotlib.get_backend()
@@ -17,10 +17,13 @@ if __name__ == '__main__':
     data1 = RadarDataR2(DIR_NAME)
     if DIR2_NAME is None:
         data_list = [data1]
+        output_file_name = DIR_NAME + ' [График R2].png'
     else:
         data2 = RadarDataR2(DIR2_NAME)
         data_list = [data1, data2]
+        output_file_name = DIR2_NAME + ' [Сравнение R2].png'
 
-    plotter = RadarR2Plotter(data_list, max_y_tick=17)
+    plotter = RadarR2Plotter(data_list, max_y_tick=28)
+    # plotter = RadarR2Plotter(data_list, max_y_tick=17)
     # plotter.show()
-    plotter.save(DIR2_NAME + ' [Сравнение R2].png')
+    plotter.save(output_file_name)
